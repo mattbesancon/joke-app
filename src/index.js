e0ea0b94a7d449f4370131592fe9be81c35ea92a
Matthieu Besancon <matthieu.besancon@gmail.com>
2022-05-02 21:22:29 +0200

    put all logical joke inside index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import createJoke from './createJoke'
 import './main.scss'
+import axios from 'axios'
+
+createJoke = () => {
+  const config = {
+    headers: {
+      accept: 'application/json',
+    },
+  }
+
+  axios.get('https://icanhazdadjoke.com', config)
+    .then(response => {
+        document.getElementById('joke').innerHTML = response.data.joke
+  })
+}
 
 document.getElementById('btn').addEventListener('click', createJoke)
 createJoke()
